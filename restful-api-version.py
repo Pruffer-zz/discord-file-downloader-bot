f3a2c8caa890610a3f7b2a7ed46d4277e2a44f56
Prüffer <pruffer@avantheim.org>
2020-11-19 18:41:21 +0100

    Added MyBB-RestfulAPI-system version

---
+++ restful-api-version.py
@@ -0,0 +1,45 @@
+import os
+import discord
+import requests
+import random
+import string
+from threading import Thread
+
+# Change values below
+serverId = # Example: 123456789123456789
+botToken = # Example: "L1ZszTkJX77WicKA27xNpooTSWqqov8y86rNbDkA"
+fileDirectory = # Example: "/home/user/Documents/", make sure to include a trailing slash
+siteUrl = # Example: "https://example.com/", make sure to include a trailing slash
+apiKey = # Example: "GyljUI0BnMo7ulS1EfNTzyQTGaJPxOjm"
+
+client = discord.Client()
+
+def download(fileUrl, filename, channel):
+	print("Downloading file: " + fileUrl)
+	path = fileDirectory + channel
+	fileDownload = requests.get(fileUrl)
+	files = {
+		'file': fileDownload.content
+	}
+	data = {
+		'json':("{"
+			"\"location\":\"" + fileDirectory + "\","
+			"\"filename\":\"" + filename + "\","
+			"\"overwrite\":\"no\""
+		"}")
+	}
+	headers = {
+		"apikey": apiKey
+	}
+	print("Sending request for: " + filename)
+	response = requests.post(siteUrl + "api.php/fileupload", files=files, data=data, headers=headers)
+	print("Request finished for: " + filename)
+	
+
+@client.event
+async def on_message(message):
+	if message.attachments and message.guild.id == serverId:
+		for file in message.attachments:
+			Thread(target=download, args=(''.join(file.url), file.filename, str(message.channel))).start()
+
+client.run(botToken)
